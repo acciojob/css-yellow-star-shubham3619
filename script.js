fe88c57f37c293094e6238675927415c841803e4
shubham3619 <120643298+shubham3619@users.noreply.github.com>
2023-05-17 08:59:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+.star::after {
+  content: "";
+  width: 0;
+  height: 0;
+  border-right: 50px solid transparent;
+  border-bottom: 35px solid yellow;
+  border-left: 50px solid transparent;
+  position: absolute;
+  top: -20px;
+  left: 50px;
+  transform: rotate(-35deg);
+}
